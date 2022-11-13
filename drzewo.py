dc17f037c97ae1180b5641f472065b6834643269
mkownak <92052799+mkownak@users.noreply.github.com>
2022-11-13 16:23:15 +0100

    Update drzewo.py

--- drzewo.py
+++ drzewo.py
@@ -1,11 +1,12 @@
-from typing import Any, List, Callable
+from typing import Any, List, Callable, Union
 import FIFO
+import graphviz
 
 class TreeNode:
     value: Any
     children: List['TreeNode']
 
-    def __init__(self, value:Any):
+    def __init__(self, value:Any)->None:
         self.value=value
         self.children=[]
 
@@ -28,25 +29,26 @@ class TreeNode:
             for child in self.children:
                 child.for_each_deep_first(visit)
 
-    def for_each_level_order(self, visit: Callable[['TreeNode'],None]): #POPRAW
+    def for_each_level_order(self,visit: Callable[['TreeNode'], None]): #POPRAW
         que=FIFO.Queue()
-        que.enqueue(self.value)
+        que.enqueue(self)
 
-        while len(que) !=0 :
+        while len(que)!=0:
             d=len(que)
 
             while d>0:
                 p=que.peek()
                 que.dequeue()
-                p.visit()
+                p.value.visit() #print(p.value.value)
 
-            for i in range(len(p.children)):
-                que.enqueue(p.children[i])
+                for i in p.value.children:
+                    que.enqueue(i)
                 d-=1
 
-    def search(self,value):
+
+    def search(self,value:Any)->Union['TreeNode', None]:
         if self.value==value:
-            return True
+            return self
 
         if len(self.children)!=0:
             for child in self.children:
@@ -55,6 +57,50 @@ class TreeNode:
                 if temp!=0:
                     return temp
         return False
+
+    def print(self)->None: #DFS
+        print(self.value)
+
+        if len(self.children) != 0:
+            for child in self.children:
+                child.print()
+
+
+
+class Tree:
+    root: TreeNode
+
+    def __init__(self,root:Any=None):
+        self.root=root
+
+    def add(self,value:Any,parent_name:Any)->None:
+        if TreeNode.search(root,parent_name)==False:
+            print("Nie mozna dodac dziecka do nieistniejacego rodzica")
+            return
+        else:
+            node=TreeNode(value)
+            parent=TreeNode.search(root,parent_name)
+            TreeNode.add(parent,node)
+
+    def for_each_level_order(self, visit: Callable[['TreeNode'], None]) -> None:
+        TreeNode.for_each_level_order(root,visit)
+
+    def for_each_deep_first(self, visit: Callable[['TreeNode'], None]) -> None:
+        TreeNode.for_each_deep_first(root,visit)
+
+    def show(self): #dokoncz
+        dot = graphviz.Digraph("Tree",format="png")
+
+        dot.node('root',str(self.root.value))
+
+        if len(self.root.children)!=0:
+            for child in self.root.children:
+                dot.node(str(child),str(child.value))
+                dot.edge('root',str(child))
+
+
+        dot.render(directory='doctest-output').replace('\\', '/')
+
 
 
 root=TreeNode("F")
@@ -69,4 +115,12 @@ root.children[1].children[0].add(TreeNode("H"))
 
 #root.for_each_deep_first(root.visit)
 #root.for_each_level_order(root.visit)
+#root.print()
+
+tree=Tree(root)
+#tree.add("K","A")
+#root.print()
+tree.show()
+#tree.for_each_level_order(tree.root.visit)
+#tree.for_each_deep_first(tree.root.visit)
 
